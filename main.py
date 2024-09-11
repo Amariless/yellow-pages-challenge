a9b19dc3e659b43d2da56b4c283d9abcb332d6d5
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-09-11 07:44:28 -0500

    cambio1

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-# Tu código aquí
+#Diccionario
+from data import people
+
+#Listar usuarios
+
+print("Todos los usuarios son: ", list(people))
+
+#Agregar usuario y numero
+
+people[input()]
+
+#Modificar usuario existente
+
+#Eliminar usuario
+
+#Salir
+
+#Menu
+
+while True:
+    print("1. Listar usuarios: ")
+    print("2. Añadir un usuario: ")
+    print("3. Modificar usuario existente: ")
+    print("4. Eliminar usuario existente: ")
+    print("5. Salir")
+
+    user_input = int(input())
+
+    if user_input == 1:
+        print ("Holiii")
+    elif user_input == 2:
+        print("Chao malparid")
+    elif user_input == 3:
+        break
+    else:
+        break
